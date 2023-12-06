8575e8ef6cb02304515288749bd55f06a43c56d8
Dustin Michels <dustin7538@gmail.com>
2023-12-06 14:40:32 -0500

    update build script

--- build.sh
+++ build.sh
@@ -3,3 +3,9 @@ GOOS=windows GOARCH=amd64 go build -o bin/connect4-amd64.exe main.go
 
 # linux
 GOOS=linux GOARCH=amd64 go build -o bin/connect4-amd64-linux main.go
+
+# mac (amd64)
+GOOS=darwin GOARCH=amd64 go build -o bin/connect4-amd64-mac main.go
+
+# mac (arm64)
+GOOS=darwin GOARCH=arm64 go build -o bin/connect4-arm64-mac main.go
